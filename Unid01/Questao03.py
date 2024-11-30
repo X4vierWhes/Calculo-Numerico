6f0785f4baea729141843385f97d31ad08d71b71
Robson Breno <robson.pinheiro@alunos.ufersa.edu.br>
2024-11-30 13:39:51 -0300

    questões 3 e 4 funcionando 100%

--- Unid01/Questao03.py
+++ Unid01/Questao03.py
@@ -1,74 +1,81 @@
-import math
 import time
-from sympy import sympify, symbols
+import math
 
-# Receber a função do usuário
-user_input = input("Digite uma função matemática de x (ex: x**2 + 2*x + 1): ")
+#variavel caso queira testar se s(x) < epsilon
+testar_funcoes_com_x = False
 
+#Definindo a função
+def s(x):
+        return (x - 1)**3
 
-def s(x0):
-    x = symbols('x')
+#Método da Bisseção
+def metodo_bissecao(a, b, epsilon):
     try:
-        # Converter a string em uma expressão simbólica
-        expr = sympify(user_input)
-
-        return expr.subs(x, x0)
-    except Exception as e:
-        print(f"Erro ao processar a função: {e}")
-
-
-def ZeroDaFuncao(a, b):
-    tempo_inicial = time.time()
-    epsilon = 10 ** -5
-    # Garantir que s(a) e s(b) não tenham sinais opostos
-    if s(a) * s(b) >= 0:
-        ite = int((math.log10(b - a) - math.log10(epsilon)) / math.log10(2)) + 1
-        k = ite
-        while (k):
-            k -= 1
-            x = (a + b) / 2
-            if s(x) == 0:
-                break
-            elif abs(s(a)) < epsilon:
-                x = a
-                break
-            elif abs(s(b)) < epsilon:
-                x = b
-                break
-            elif (abs(s(a)) < abs(s(b))):
-                b = x
-            else:
-                a = x
-
-        if (s(x) > epsilon):
-            print('Não existe raízes entre o intervalo dado.')
-        else:
-            tempo_final = time.time()
-            print('Tempo em segundos: ', (tempo_final - tempo_inicial))
-            print('Iterações necessárias: ', (ite - k))
-            print(f'A raiz aproximada é: {x:.5f}')
-
-    else:
+        tempo_inicial = time.time()
         ite = 0
-        while abs(b - a) > epsilon:
-            ite += 1
-            x = (a + b) / 2
-            if s(x) == 0:
-                break
-            elif s(a) * s(x) < 0:
-                b = x
+
+        # Garantir que s(a) e s(b) não tenham sinais opostos
+        if s(a) * s(b) >= 0:
+            k = int((math.log10(b - a) - math.log10(epsilon))/math.log10(2)) + 1
+            
+            while(k):
+                k-=1
+                ite += 1
+                x = (a + b) / 2
+                if s(x) == 0:
+                    break
+                elif abs(s(a)) < epsilon:
+                    x = a
+                    break
+                elif abs(s(b)) < epsilon:
+                    x = b
+                    break
+                elif (abs(s(a)) < abs(s(b))): 
+                    b = x
+                else:
+                    a = x
+
+            if(s(x) > epsilon):
+                print('Não existe raízes entre o intervalo dado.')
             else:
-                a = x
+                print("\n[Método da Bisseção]")
+                print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+                print(f"Iterações necessárias: {ite}")
+                print(f"A raiz aproximada é: {x:.5f}")
+                if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
 
-        tempo_final = time.time()
-        print('Tempo em segundos: ', (tempo_final - tempo_inicial))
-        print('Iterações necessárias: ', (ite))
-        print(f"A raiz aproximada é: {x:.5f}")
+        else:
+            while abs(b - a) > epsilon:
+                ite += 1
+                x = (a + b) / 2
+                if s(x) == 0:
+                    break
+                elif s(a) * s(x) < 0:
+                    b = x
+                else:
+                    a = x
 
+            print("\n[Método da Bisseção]")
+            print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+            print(f"Iterações necessárias: {ite}")
+            print(f"A raiz aproximada é: {x:.5f}")
+            if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
 
-# ...
-a = int(input('Digite o início do intervalo: '))
-b = int(input('Digite o fim do intervalo: '))
+    except Exception as e:
+        print('\nIntervalo inválido no Método da Bisseção, digite outro intervalo.')
+        #Recebe um novo intervalo do usuário
+        a = float(input('Digite o início do intervalo: '))
+        b = float(input('Digite o fim do intervalo: '))
 
-ZeroDaFuncao(a, b)
+        #Executa o metodo novamente
+        metodo_bissecao(a, b, epsilon)
 
+#Dados iniciais
+a = float(input('Digite o início do intervalo: '))
+b = float(input('Digite o fim do intervalo: '))
+epsilon = float(input('Digite o valor de epsilon: '))
+
+#Executa o método
+metodo_bissecao(a, b, epsilon)
--- Unid01/Questao04.py
+++ Unid01/Questao04.py
@@ -1,75 +1,151 @@
-import math
 import time
-from sympy import sympify, symbols
+import math
 
-# Receber a função do usuário
-user_input = input("Digite uma função matemática de x (ex: x**2 - 4): ")
+#variavel caso queira testar se s(x) < epsilon
+testar_funcoes_com_x = False
 
-epsilon = 0.00001
+#Definindo a função
+def s(x):
+        return (x - 1)**3
 
-def s(x0):
-    x = symbols('x')
+#Método da Falsa Posição
+def metodo_falsa_posicao(a, b, epsilon):
     try:
-        # Converter a string em uma expressão simbólica
-        expr = sympify(user_input)
-        return expr.subs(x, x0)
+        tempo_inicial = time.time()
+        ite = 0
+
+        # Garantir que s(a) e s(b) não tenham sinais opostos
+        if s(a) * s(b) >= 0:
+            k = 100
+            while(k > 0):
+                k -= 1
+                ite += 1
+                x =  (a * s(b) - b * s(a)) / (s(b) - s(a))
+                if abs(s(x)) < epsilon:
+                    break
+                elif abs(s(a)) < epsilon:
+                    x = a
+                    break
+                elif abs(s(b)) < epsilon:
+                    x = b
+                    break
+                elif (abs(s(a)) < abs(s(b))): 
+                    b = x #Raiz está entre a e x
+                else:
+                    a = x #Raiz está entre x e b
+
+            if(s(x) > epsilon):
+                print('Não existem raízes entre o intervalo dado.')
+            else:
+                print("\n[Método da Falsa Posição]")
+                print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+                print(f"Iterações necessárias: {ite}")
+                print(f"A raiz aproximada é: {x:.5f}") 
+                if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
+
+        else:
+            while (abs(b - a) > epsilon):
+                ite += 1
+                x = (a * s(b) - b * s(a)) / (s(b) - s(a)) # Fórmula da falsa posição
+                if s(x) == 0:
+                    break
+                elif abs(s(a)) < epsilon:
+                    x = a
+                    break
+                elif abs(s(b)) < epsilon:
+                    x = b
+                    break
+                elif abs(s(a)) < abs(s(b)):
+                    b = x # Raiz está entre a e x
+                else:
+                    a = x # Raiz está entre x e b
+
+            print("\n[Método da Falsa Posição]")
+            print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+            print(f"Iterações necessárias: {ite}")
+            print(f"A raiz aproximada é: {x:.5f}")   
+            if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
     except Exception as e:
-        print(f"Erro ao processar a função: {e}")
+        print('\nIntervalo inválido no Método da Falsa Posição, digite outro intervalo.')
+        #Recebe um novo intervalo do usuário
+        a = float(input('Digite o início do intervalo: '))
+        b = float(input('Digite o fim do intervalo: '))
 
-# Método da Bisseção
-def metodo_bissecao(a, b):
-    tempo_inicial = time.time()
-    ite = 0
+        #Executa o metodo novamente
+        metodo_falsa_posicao(a, b, epsilon)
 
-    if s(a) * s(b) >= 0:
-        print("Erro: s(a) e s(b) devem ter sinais opostos.")
-        return
+#Método da Bisseção
+def metodo_bissecao(a, b, epsilon):
+    try:
+        tempo_inicial = time.time()
+        ite = 0
 
-    while abs(b - a) > epsilon:
-        ite += 1
-        x = (a + b) / 2
-        if s(x) == 0:
-            break
-        elif s(a) * s(x) < 0:
-            b = x
+        # Garantir que s(a) e s(b) não tenham sinais opostos
+        if s(a) * s(b) >= 0:
+            k = int((math.log10(b - a) - math.log10(epsilon))/math.log10(2)) + 1
+            
+            while(k):
+                k-=1
+                ite += 1
+                x = (a + b) / 2
+                if s(x) == 0:
+                    break
+                elif abs(s(a)) < epsilon:
+                    x = a
+                    break
+                elif abs(s(b)) < epsilon:
+                    x = b
+                    break
+                elif (abs(s(a)) < abs(s(b))): 
+                    b = x
+                else:
+                    a = x
+
+            if(s(x) > epsilon):
+                print('Não existe raízes entre o intervalo dado.')
+            else:
+                print("\n[Método da Bisseção]")
+                print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+                print(f"Iterações necessárias: {ite}")
+                print(f"A raiz aproximada é: {x:.5f}")
+                if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
+
         else:
-            a = x
+            while abs(b - a) > epsilon:
+                ite += 1
+                x = (a + b) / 2
+                if s(x) == 0:
+                    break
+                elif s(a) * s(x) < 0:
+                    b = x
+                else:
+                    a = x
 
-    tempo_final = time.time()
-    print("\n[Método da Bisseção]")
-    print(f"Tempo de execução: {tempo_final - tempo_inicial:.6f} segundos")
-    print(f"Iterações necessárias: {ite}")
-    print(f"A raiz aproximada é: {x:.5f}")
+            print("\n[Método da Bisseção]")
+            print(f"Tempo de execução: {time.time() - tempo_inicial:.6f} segundos")
+            print(f"Iterações necessárias: {ite}")
+            print(f"A raiz aproximada é: {x:.5f}")
+            if(testar_funcoes_com_x):
+                    print(f's(x) é menor que epsilon? {s(x) < epsilon}')
 
-# Método da Falsa Posição
-def metodo_falsa_posicao(a, b):
-    tempo_inicial = time.time()
-    ite = 0
+    except Exception as e:
+        print('\nIntervalo inválido no Método da Bisseção, digite outro intervalo.')
+        #Recebe um novo intervalo do usuário
+        a = float(input('Digite o início do intervalo: '))
+        b = float(input('Digite o fim do intervalo: '))
 
-    if s(a) * s(b) >= 0:
-        print("Erro: s(a) e s(b) devem ter sinais opostos.")
-        return
+        #Executa o metodo novamente
+        metodo_bissecao(a, b, epsilon)
 
-    while abs(s((a * s(b) - b * s(a)) / (s(b) - s(a)))) > epsilon:
-        ite += 1
-        x = (a * s(b) - b * s(a)) / (s(b) - s(a))
-        if s(x) == 0:
-            break
-        elif s(a) * s(x) < 0:
-            b = x
-        else:
-            a = x
 
-    tempo_final = time.time()
-    print("\n[Método da Falsa Posição]")
-    print(f"Tempo de execução: {tempo_final - tempo_inicial:.6f} segundos")
-    print(f"Iterações necessárias: {ite}")
-    print(f"A raiz aproximada é: {x:.5f}")
-
-# Receber intervalo do usuário
+#Dados iniciais
 a = float(input('Digite o início do intervalo: '))
 b = float(input('Digite o fim do intervalo: '))
+epsilon = float(input('Digite o valor de epsilon: '))
 
-# Executar os métodos
-metodo_bissecao(a, b)
-metodo_falsa_posicao(a, b)
+#Executa os métodos
+metodo_bissecao(a, b, epsilon)
+metodo_falsa_posicao(a, b, epsilon)
